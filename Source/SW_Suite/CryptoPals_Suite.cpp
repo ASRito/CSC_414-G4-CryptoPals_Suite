a1125b621686300a216711e66bea26dcb7b243b6
ASRito <w10098787@usm.edu>
2024-12-04 20:00:42 -0600

    Undo CryptoPals_Suite.cpp

--- Source/SW_Suite/CryptoPals_Suite.cpp
+++ Source/SW_Suite/CryptoPals_Suite.cpp
@@ -163,17 +163,7 @@ void Task7Eval()
 //        Electronic CodeBook (ECB) encryption amongst surplus data
 void Task8Eval()
 {
-    string inputFile = "ECBDetectFile.txt";
-    string inputTextA = ECBDET_INPUT_TEMPLATE;
-
-    cout << endl << endl << endl << endl;
-    cout << "Task 8 Evaluation: _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
-    cout << "Input File name: " << inputFile << endl << "..." << endl;
-    cout << "Input File Contents... " << endl << inputTextA << endl << endl;
-    bool is_ecb = detect_ecb(inputTextA);
-    cout << ((is_ecb) ? "" : "NO ") << "ECB DETECTED" << endl;
-    cout << "Expected: Yes, ECB encryption is present." << endl;
-    cout << " _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl << endl << endl;
+    //INSERT TASK 8 EVAL CODE HERE
 }
 
 }
